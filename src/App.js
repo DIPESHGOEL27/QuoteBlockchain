3e13e25d8210ac28d275fbfb5275dfb8468d236b
Dipesh Goel <118828297+DIPESHGOEL27@users.noreply.github.com>
2023-08-08 00:26:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = ()=> {
       </main>
       <footer className='Footer'>
       <p className='copyright'>COPYRIGHT &copy; {new Date().getFullYear()}, ALL RIGHTS RESERVED</p>
-      <p>Made with ❤️ by <a href='https://6rmoq-4iaaa-aaaak-aezja-cai.icp0.io/' target='_blank' rel='noreferrer'>Dipesh Goel</a><i>  under mentorship of </i> <a href='https://www.linkedin.com/in/kanishkkhurana/' target='_blank' rel='noreferrer'>Kanishk Sir</a> ( <a href='https://academy.lumoslabs.co/' target='_blank' rel='noreferrer'>Lumos Academy</a> )  using <img src={react_logo} alt='React Logo' className='React-logo'/> </p>
+      <p>Made with ❤️ by <a href='https://6rmoq-4iaaa-aaaak-aezja-cai.icp0.io/' target='_blank' rel='noreferrer'>Dipesh Goel</a><i>  under mentorship of </i> <a href='https://www.linkedin.com/in/hardik-singh-36ba06195/?originalSubdomain=in' target='_blank' rel='noreferrer'>Hardik Sir</a> ( <a href='https://academy.lumoslabs.co/' target='_blank' rel='noreferrer'>Lumos Academy</a> )  using <img src={react_logo} alt='React Logo' className='React-logo'/> </p>
       </footer>
       
     </div>
